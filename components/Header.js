5aaefeb300ef69a11953ea510e10f73abd3343ea
Nikolay Antonovich <TesaloNick@gmail.com>
2021-04-11 23:51:26 +0300

    add basket fitch

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 import $ from './utilits.js'
+import {getCookie} from '../cookie.js'
+import {setCookie} from '../cookie.js'
 class Header {
     create() {
         const header = document.createElement('header')
@@ -8,8 +10,23 @@ class Header {
         logo.innerHTML = `<img class='logo' src='images/metro-logo.png'>`
         header.insertAdjacentElement('afterbegin', logo)
         const basket = document.createElement('a')
+        basket.classList.add('basket-link')
         basket.setAttribute('href', '#')
-        basket.innerHTML = `<img class='basket' src='images/basket.png'>`
+        let sum = 0
+        if (localStorage.getItem('buyProduct')) {
+            JSON.parse(localStorage.getItem('buyProduct')).map(item => {
+                sum += item.amount * item.price
+                console.log(sum);
+            })
+            setCookie('fullPrice', sum)
+            basket.innerHTML = `<img class='img-basket' src='images/basket.png'>
+            <span class='amount-products-basket'>${JSON.parse(localStorage.getItem('buyProduct')).length}</span>
+            <span class='full-price-basket'>$${+Math.round(sum*100)/100}</span>`
+        } else {
+            basket.innerHTML = `<img class='img-basket' src='images/basket.png'>
+            <span class='amount-products-basket'>0</span>
+            <span class='full-price-basket'>$0</span>`
+        }
         header.insertAdjacentElement('beforeend', basket)
     }
     init() {
--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,13 @@
 import $ from './utilits.js'
+import {getCookie} from '../cookie.js'
+import {setCookie} from '../cookie.js'
 
 class Main {
+    constructor() {
+        this.amountProduct = []
+        this.buyProducts = localStorage.getItem('buyProduct') ? JSON.parse(localStorage.getItem('buyProduct')) : []
+        this.fullPrice = localStorage.getItem('buyProduct') ? getCookie('fullPrice') : 0
+    }
     create() {
         const mainHead = document.createElement('div')
         mainHead.classList.add('products-head')
@@ -19,32 +26,51 @@ class Main {
                 <h2>${product.title}</h2>
                 <div class ='product-basket'>
                     <p class='price'>$${product.price}</p>
-                    <img src='images/basket.png'>
+                    <img class='buy-button' src='images/basket.png'>
                 </div>
                 
             </div>
 
         </div>
         `)
+
     }
-    amountBuyProduct(){
-        const products = document.querySelectorAll(".product");
-        let arrAmountBuyProduct = new Array(20);
-        arrAmountBuyProduct.fill(1)
-        console.log(arrAmountBuyProduct);
+    getAmountBuyProduct(){
+        const products = document.querySelectorAll(".product"); // массив всех продуктов
+        const buttonForBuy = document.querySelectorAll(".buy-button"); // массив всех кнопок покупки
+        this.amountProduct = new Array(20);
+        this.amountProduct.fill(1)
         for (let i = 0; i < products.length; i++) {
             products[i].addEventListener("mouseenter", () => {
-                console.log(arrAmountBuyProduct);
                 products[i].querySelector('.price').innerHTML = `
-                <input type="number" class='amount${i}' value='${arrAmountBuyProduct[i]}'>
+                <input type="number" class='amount${i}' value='${this.amountProduct[i]}'>
                 `
-            });
+            }); 
             products[i].addEventListener("mouseleave", () => {
-                arrAmountBuyProduct[i] = +$(`.amount${i}`).value
+                this.amountProduct[i] = +$(`.amount${i}`).value
                 products[i].querySelector('.price').innerHTML = `
                 <p class='price'>$${JSON.parse(localStorage.getItem('products'))[i].price}</p>
                 `
             });
+            buttonForBuy[i].addEventListener('click', () => { // событие по нажатию на кнопку покупки
+                let buyObj = JSON.parse(localStorage.getItem('products'))[i]
+                buyObj.amount = +$(`.amount${i}`).value // добавляем свойство в метод с количеством купленных товаров
+
+                if (this.buyProducts.find(item => item.title === buyObj.title)) { // проверка на повтор уже довабленного продукта
+                    let repeat = this.buyProducts.find(item => item.title === buyObj.title)
+                    repeat.amount += buyObj.amount
+                } else {
+                    this.buyProducts.push(buyObj) 
+                }
+
+                $('.amount-products-basket').innerHTML = this.buyProducts.length                
+                this.fullPrice += buyObj.amount * buyObj.price // расчет общей стоимости
+                localStorage.setItem('buyProduct', JSON.stringify(this.buyProducts)) 
+                setCookie('fullPrice', this.fullPrice)   // добавляем в куки общую стоимость
+                $('.full-price-basket').innerHTML = '$' + Math.round(this.fullPrice*100)/100
+                $(`.amount${i}`).value = 1 // сброс цифры покупки
+                console.log(this.buyProducts);
+            })
         }
     }
     getProducts(){
@@ -52,23 +78,21 @@ class Main {
             fetch('https://fakestoreapi.com/products')
             .then(response => response.json())
             .then(products => {
-                console.log(products)
                 localStorage.setItem('products', JSON.stringify(products))
                 products.map(product => this.createProductCard(product))
-                this.amountBuyProduct()
+                this.getAmountBuyProduct()
             })
         } else {
             JSON.parse(localStorage.getItem('products')).map(product => this.createProductCard(product))
-            this.amountBuyProduct()
+            this.getAmountBuyProduct()
         }
+    }
+    addProductInBasket() {
 
-  
     }
     init() {
-
         this.create()
         this.getProducts()
-        
     }
 }
 const main = new Main().init()
